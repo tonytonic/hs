a6ffbf767d989c3f848c9c37dd572f2f9745142f
tonytonic <anthony.chauvel29@orange.fr>
2026-03-07 13:07:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /**
  * Service Worker : Simulateur Heures Sup & RPG Fox
- * Version : 1.7.2 (Le Parfait - Offline Solide + Periodic Sync)
+ * Version : 1.7.3 (Le Parfait - Offline Solide + Periodic Sync)
  */
 
-const CACHE_NAME = "heuressup-cache-v1.7.2";
+const CACHE_NAME = "heuressup-cache-v1.7.3";
 const OFFLINE_URL = "./menu.html";
 
 const FILES_TO_CACHE = [
